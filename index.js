0b704d9820c5982145615b43f26133aca8232010
leonardodamasceno8 <leonardodamasceno8@users>
2019-10-14 18:17:39 -0300

    setando para utilizar o ZEIT

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+const path = require('path')
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname,'public')))
+
+const port = process.env.PORT || 3000
 
 app.get('/', (Request,Response) => {
     Response.render('home')
@@ -13,7 +17,7 @@ app.get('/info', (Request,Response) => {
     Response.render('info')
 })
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if(err){
         console.log("Nao foi possivel conectar no servidor")
     }else {
